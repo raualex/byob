2a63d1a3f2adbb091e3fba6cb42254aa97249424
Alexander Rau <alexanderrau86@yahoo.com>
2018-12-04 14:47:54 -0700

    Fix integer columns in migration

--- db/seeds/dev/cities.js
+++ db/seeds/dev/cities.js
@@ -6,12 +6,12 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       return Promise.all([
         knex('cities').insert({
-          city: 'Denver', state: 'CO', primary_airport: 'Denver International Airport', population: '1,000,000', tourism_website: 'www.denver.com'
+          city: 'Denver', state: 'CO', primary_airport: 'Denver International Airport', population: 1000000, tourism_website: 'www.denver.com'
         }, 'id')
         .then(city => {
           return knex('comedy_clubs').insert([
-            { name: 'Denver Comedy Club', street_address: '123 Denver St.', zip_code: '80013', rating: 5, city_id: city[0] },
-            { name: 'Comedy Works', street_address: '321 Not Denver St.', zip_code: '80212', rating: 4, city_id: city[0] }
+            { name: 'Denver Comedy Club', street_address: '123 Denver St.', zip_code: 80013, rating: 5, city_id: city[0] },
+            { name: 'Comedy Works', street_address: '321 Not Denver St.', zip_code: 80212, rating: 4, city_id: city[0] }
           ])
         })
         .then(() => console.log('Seeding complete!'))
