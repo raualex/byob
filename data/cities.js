a1824d980f7f8c93728fc6c5c3cc410aeb0ea743
Alexander Rau <alexanderrau86@yahoo.com>
2018-12-04 15:42:11 -0700

    Clean up city data

--- data/cities.js
+++ data/cities.js
@@ -21,192 +21,192 @@ const cities = [
       tourism_website: 'https://www.cityofchicago.org'
     },
     {
-        city: 'Atlanta',
-        state: 'GA',
-        primary_airport: 'Hartsfield-Jackson Atlanta International Airport',
-        population: 486290,
-        tourism_website: 'https://www.atlanta.com'
-      },
-      {
-        city: 'Detroit',
-        state: 'MI',
-        primary_airport: 'Detroit Metropolitan Airport',
-        population: 673104,
-        tourism_website: 'https://www.visitdetroit.com'
-      },
-      {
-        city: 'Los Angeles',
-        state: 'CA',
-        primary_airport: 'Los Angeles International Airport',
-        population: 4000000,
-        tourism_website: 'https://www.discoverlosangeles.com'
-      },
-      {
-        city: 'Philadelphia',
-        state: 'PA',
-        primary_airport: 'Philadelphia International Airport',
-        population: 1581000,
-        tourism_website: 'https://www.visitphilly.com'
-      },
-      {
-        city: 'Boston',
-        state: 'MA',
-        primary_airport: 'Logan International Airport',
-        population: 685094,
-        tourism_website: 'https://www.bostonusa.com'
-      },
-      {
-        city: 'Miami',
-        state: 'FL',
-        primary_airport: 'Miami International Airport',
-        population: 463347,
-        tourism_website: 'https://www.visitflorida.com'
-      },
-      {
-        city: 'Minneapolis',
-        state: 'MN',
-        primary_airport: 'Minneapolis-Saint Paul International Airport',
-        population: 422331,
-        tourism_website: 'https://www.minneapolis.org'
-      },
-      {
-        city: 'Austin',
-        state: 'TX',
-        primary_airport: 'Austin-Bergstrom International Airport',
-        population: 950715,
-        tourism_website: 'https://www.austintexas.org'
-      },
-      {
-        city: 'Dallas',
-        state: 'TX',
-        primary_airport: 'Dallas/Fort Worth International Airport',
-        population: 1341000,
-        tourism_website: 'https://www.visitdallas.com'
-      },
-      {
-        city: 'Seattle',
-        state: 'WA',
-        primary_airport: 'Seattle-Tacoma International Airport',
-        population: 724745,
-        tourism_website: 'https://www.visitseattle.org'
-      },
-      {
-          city: 'San Diego',
-          state: 'CA',
-          primary_airport: 'San Diego International Airport',
-          population: 1420000,
-          tourism_website: 'https://www.sandiego.org'
-        },
-        {
-          city: 'Las Vegas',
-          state: 'NV',
-          primary_airport: 'McCarran International Airport',
-          population: 641676,
-          tourism_website: 'https://www.visitlasvegas.com'
-        },
-        {
-          city: 'Baltimore',
-          state: 'MD',
-          primary_airport: 'Baltimore-Washington International Airport',
-          population: 611648,
-          tourism_website: 'https://www.baltimore.org'
-        },
-        {
-          city: 'Nashville',
-          state: 'TN',
-          primary_airport: 'Nashville International Airport',
-          population: 691243,
-          tourism_website: 'https://www.visitmusiccity.com'
-        },
-        {
-          city: 'Louisville',
-          state: 'KY',
-          primary_airport: 'Louisville International Airport',
-          population: 616261,
-          tourism_website: 'https://www.gotolouisville.com'
-        },
-        {
-          city: 'New Orleans',
-          state: 'LA',
-          primary_airport: 'Louis Armstrong New Orleans International Airport',
-          population: 393292,
-          tourism_website: 'https://www.neworleans.com'
-        },
-        {
-          city: 'Charlotte',
-          state: 'NC',
-          primary_airport: 'Charlotte Douglas International Airport',
-          population: 859035,
-          tourism_website: 'https://www.visitnc.com'
-        },
-        {
-            city: 'Cleveland',
-            state: 'OH',
-            primary_airport: 'Cleveland Hopkins International Airport',
-            population: 385525,
-            tourism_website: 'https://www.thisiscleveland.com'
-          },
-          {
-            city: 'Indianapolis',
-            state: 'IN',
-            primary_airport: 'Indianapolis International Airport',
-            population: 872680,
-            tourism_website: 'https://www.visitindy.com'
-          },
-          {
-            city: 'Phoenix',
-            state: 'AZ',
-            primary_airport: 'Phoenix Sky Harbor International Airport',
-            population: 1626000,
-            tourism_website: 'https://www.visitphoenix.com'
-          },
-          {
-              city: 'Kansas City',
-              state: 'MO',
-              primary_airport: 'Kansas City International Airport',
-              population: 488943,
-              tourism_website: 'https://www.visitkc.com'
-            },
-            {
-              city: 'Cincinnati',
-              state: 'OH',
-              primary_airport: 'Cincinnati Municipal Airport',
-              population: 301301,
-              tourism_website: 'https://www.cincinnatiusa.com'
-            },
-            {
-              city: 'Oklahoma City',
-              state: 'OK',
-              primary_airport: 'Will Rogers World Airport',
-              population: 643648,
-              tourism_website: 'https://www.visitokc.com'
-            },
-            {
-              city: 'Houston',
-              state: 'TX',
-              primary_airport: 'George Bush International Airport',
-              population: 2313000,
-              tourism_website: 'https://www.visithoustontexas.com'
-            },
-            {
-              city: 'San Francisco',
-              state: 'CA',
-              primary_airport: 'San Francisco International Airport',
-              population: 884363,
-              tourism_website: 'https://www.sftravel.com'
-            },
-            {
-              city: 'Portland',
-              state: 'OR',
-              primary_airport: 'Portland International Airport',
-              population: 647805,
-              tourism_website: 'https://www.travelportland.com'
-            },
-            {
-              city: 'Milwaukee',
-              state: 'WI',
-              primary_airport: 'General Mitchell International Airport',
-              population: 595351,
-              tourism_website: 'https://www.visitmilwaukee.org'
-            }
+      city: 'Atlanta',
+      state: 'GA',
+      primary_airport: 'Hartsfield-Jackson Atlanta International Airport',
+      population: 486290,
+      tourism_website: 'https://www.atlanta.com'
+    },
+    {
+      city: 'Detroit',
+      state: 'MI',
+      primary_airport: 'Detroit Metropolitan Airport',
+      population: 673104,
+      tourism_website: 'https://www.visitdetroit.com'
+    },
+    {
+      city: 'Los Angeles',
+      state: 'CA',
+      primary_airport: 'Los Angeles International Airport',
+      population: 4000000,
+      tourism_website: 'https://www.discoverlosangeles.com'
+    },
+    {
+      city: 'Philadelphia',
+      state: 'PA',
+      primary_airport: 'Philadelphia International Airport',
+      population: 1581000,
+      tourism_website: 'https://www.visitphilly.com'
+    },
+    {
+      city: 'Boston',
+      state: 'MA',
+      primary_airport: 'Logan International Airport',
+      population: 685094,
+      tourism_website: 'https://www.bostonusa.com'
+    },
+    {
+      city: 'Miami',
+      state: 'FL',
+      primary_airport: 'Miami International Airport',
+      population: 463347,
+      tourism_website: 'https://www.visitflorida.com'
+    },
+    {
+      city: 'Minneapolis',
+      state: 'MN',
+      primary_airport: 'Minneapolis-Saint Paul International Airport',
+      population: 422331,
+      tourism_website: 'https://www.minneapolis.org'
+    },
+    {
+      city: 'Austin',
+      state: 'TX',
+      primary_airport: 'Austin-Bergstrom International Airport',
+      population: 950715,
+      tourism_website: 'https://www.austintexas.org'
+    },
+    {
+      city: 'Dallas',
+      state: 'TX',
+      primary_airport: 'Dallas/Fort Worth International Airport',
+      population: 1341000,
+      tourism_website: 'https://www.visitdallas.com'
+    },
+    {
+      city: 'Seattle',
+      state: 'WA',
+      primary_airport: 'Seattle-Tacoma International Airport',
+      population: 724745,
+      tourism_website: 'https://www.visitseattle.org'
+    },
+    {
+      city: 'San Diego',
+      state: 'CA',
+      primary_airport: 'San Diego International Airport',
+      population: 1420000,
+      tourism_website: 'https://www.sandiego.org'
+    },
+    {
+      city: 'Las Vegas',
+      state: 'NV',
+      primary_airport: 'McCarran International Airport',
+      population: 641676,
+      tourism_website: 'https://www.visitlasvegas.com'
+    },
+    {
+      city: 'Baltimore',
+      state: 'MD',
+      primary_airport: 'Baltimore-Washington International Airport',
+      population: 611648,
+      tourism_website: 'https://www.baltimore.org'
+    },
+    {
+      city: 'Nashville',
+      state: 'TN',
+      primary_airport: 'Nashville International Airport',
+      population: 691243,
+      tourism_website: 'https://www.visitmusiccity.com'
+    },
+    {
+      city: 'Louisville',
+      state: 'KY',
+      primary_airport: 'Louisville International Airport',
+      population: 616261,
+      tourism_website: 'https://www.gotolouisville.com'
+    },
+    {
+      city: 'New Orleans',
+      state: 'LA',
+      primary_airport: 'Louis Armstrong New Orleans International Airport',
+      population: 393292,
+      tourism_website: 'https://www.neworleans.com'
+    },
+    {
+      city: 'Charlotte',
+      state: 'NC',
+      primary_airport: 'Charlotte Douglas International Airport',
+      population: 859035,
+      tourism_website: 'https://www.visitnc.com'
+    },
+    {
+      city: 'Cleveland',
+      state: 'OH',
+      primary_airport: 'Cleveland Hopkins International Airport',
+      population: 385525,
+      tourism_website: 'https://www.thisiscleveland.com'
+    },
+    {
+      city: 'Indianapolis',
+      state: 'IN',
+      primary_airport: 'Indianapolis International Airport',
+      population: 872680,
+      tourism_website: 'https://www.visitindy.com'
+    },
+    {
+      city: 'Phoenix',
+      state: 'AZ',
+      primary_airport: 'Phoenix Sky Harbor International Airport',
+      population: 1626000,
+      tourism_website: 'https://www.visitphoenix.com'
+    },
+    {
+      city: 'Kansas City',
+      state: 'MO',
+      primary_airport: 'Kansas City International Airport',
+      population: 488943,
+      tourism_website: 'https://www.visitkc.com'
+    },
+    {
+      city: 'Cincinnati',
+      state: 'OH',
+      primary_airport: 'Cincinnati Municipal Airport',
+      population: 301301,
+      tourism_website: 'https://www.cincinnatiusa.com'
+    },
+    {
+      city: 'Oklahoma City',
+      state: 'OK',
+      primary_airport: 'Will Rogers World Airport',
+      population: 643648,
+      tourism_website: 'https://www.visitokc.com'
+    },
+    {
+      city: 'Houston',
+      state: 'TX',
+      primary_airport: 'George Bush International Airport',
+      population: 2313000,
+      tourism_website: 'https://www.visithoustontexas.com'
+    },
+    {
+      city: 'San Francisco',
+      state: 'CA',
+      primary_airport: 'San Francisco International Airport',
+      population: 884363,
+      tourism_website: 'https://www.sftravel.com'
+    },
+    {
+      city: 'Portland',
+      state: 'OR',
+      primary_airport: 'Portland International Airport',
+      population: 647805,
+      tourism_website: 'https://www.travelportland.com'
+    },
+    {
+      city: 'Milwaukee',
+      state: 'WI',
+      primary_airport: 'General Mitchell International Airport',
+      population: 595351,
+      tourism_website: 'https://www.visitmilwaukee.org'
+    }
 ]
